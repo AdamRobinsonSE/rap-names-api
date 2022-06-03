834ed87ec57a9cc2eb897ad1e1b07ca93cf4c48f
Adam Robinson <robinson.db@gmail.com>
2022-06-02 21:00:11 -0400

    first commit

---
+++ server.js
@@ -0,0 +1,38 @@
+const express = require('express');
+const app = express();
+const PORT = 8000;
+
+let rappers = {
+    '21 savage': {
+        'age': 29,
+        'birthName': 'Shéyaa Bin Abraham-Joseph',
+        'birthLocation': 'London, England',
+    },
+    'chance the rapper': {
+        'age': 28,
+        'birthName': 'Chancelor Jonathan Bennett',
+        'birthLocation': 'London, England',
+    },
+    'unknown': {
+        'age': 0,
+        'birthName': 'unknown',
+        'birthLocation': 'unknown',
+    },
+};
+
+app.get('/', (req, res) => {
+    res.sendFile(__dirname + '/index.html')
+});
+
+app.get('/api/:name', (req, res) => {
+    const rapperName = req.params.name.toLowerCase()
+    if(rappers[rapperName]){
+        res.json(rappers[rapperName])
+    }else{
+        res.json(rappers['unknown'])
+    }
+});
+
+app.listen(process.env.PORT || PORT, () => {
+    console.log(`The server is running on port ${PORT}.`)
+});
